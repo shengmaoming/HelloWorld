cae5b0abf2e724ab9b29d8d4f1bd381aaba2f437
shengmaoming <shengmaoming@163.com>
2018-03-09 14:39:13 +0800

    111

---
+++ src/main/java/com/taiji/eurekaservice/EurekaServiceApplication.java
@@ -0,0 +1,24 @@
+package com.taiji.eurekaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaServer
+@RestController
+public class EurekaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServiceApplication.class, args);
+	}
+
+	@RequestMapping("/hello")
+	public String home(){
+		return "hello World";
+	}
+
+
+}
